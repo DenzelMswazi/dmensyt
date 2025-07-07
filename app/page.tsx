032066b9d8dc89e05c59c7004829a377226ba2b5
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-07-07 22:39:12 +0300

    feat: add a button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 const Home = () => {
   return (
   <div className="p-10 bg-shop-light-pink">
     <h2>Home</h2>
+    <Button>Check Out</Button>
     </div>
   )
 }
