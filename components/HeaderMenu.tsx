764504caf825ba8748470d1253b6c5da66e14a3a
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-07-09 13:03:27 +0300

    feat: mapping the data inside the header menu component

--- components/HeaderMenu.tsx
+++ components/HeaderMenu.tsx
@@ -1,9 +1,15 @@
+import { headerData } from '@/constants/data';
+import Link from 'next/link';
 import React from 'react'
 
 const HeaderMenu = () => {
   return (
     <div>
-        
+        {headerData.map((item) => (
+            <Link key={item.title} href={item.href}>
+              {item.title}
+            </Link>
+        ))}
     </div>
   )
 }
