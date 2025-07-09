57039d6b3ddcfed8d4f08d6aa2f1c1a5b11d096a
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-07-09 13:13:15 +0300

    feat: inline header styling

--- components/HeaderMenu.tsx
+++ components/HeaderMenu.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const HeaderMenu = () => {
   return (
-    <div>
+    <div className='hidden md:inline-flex w-1/3 items-center gap-7 text-sm capitalize font-semibold text-lightColor'>
         {headerData.map((item) => (
             <Link key={item.title} href={item.href}>
               {item.title}
