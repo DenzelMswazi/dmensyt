5864556088ab179383907ee4eb33e7b147c8be64
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-07-09 12:40:05 +0300

    feat: create the header menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Container from './Container';
+import Logo from './Logo';
 
 const Header = () => {
   return (
     <header className='bg-white py-5'>
         <Container>
             {/* Logo */}
+            <Logo />
             {/* NavButton */}
             {/* NavButton */}
         </Container>
