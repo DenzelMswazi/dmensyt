6de9df2142e13e2346c205f2db455af6ecb5d225
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-07-14 10:12:04 +0300

    chore: add a pathname

--- components/HeaderMenu.tsx
+++ components/HeaderMenu.tsx
@@ -1,8 +1,10 @@
 import { headerData } from '@/constants/data';
 import Link from 'next/link';
-import React from 'react'
+import React, { use } from 'react'
 
 const HeaderMenu = () => {
+  const pathname= usePathname();
+  console.log(pathname);
   return (
     <div className='hidden md:inline-flex w-1/3 items-center gap-7 text-sm capitalize font-semibold text-lightColor'>
         {headerData.map((item) => (
