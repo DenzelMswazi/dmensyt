af0d2ce46852bf89d6b85f90c981a7653fade3ae
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-07-14 10:25:02 +0300

    chore: remove the condole log

--- components/HeaderMenu.tsx
+++ components/HeaderMenu.tsx
@@ -7,7 +7,6 @@ import { usePathname } from 'next/navigation';
 
 const HeaderMenu = () => {
   const pathname= usePathname();
-  console.log(pathname);
 
   return (
     <div className='hidden md:inline-flex w-1/3 items-center gap-7 text-sm capitalize font-semibold text-lightColor'>
