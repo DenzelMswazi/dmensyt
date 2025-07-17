99f573280a4cc3aec3e440841b5cbab707e06b23
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-07-17 14:26:30 +0300

    feat: add x icon from lucide react

--- components/SideMenu.tsx
+++ components/SideMenu.tsx
@@ -1,4 +1,6 @@
 import React, { FC } from 'react';
+import Logo from './Logo';
+import { X } from 'lucide-react';
 
 interface SidebarProps {
     isOpen: boolean;
@@ -7,7 +9,14 @@ interface SidebarProps {
 
 const SideMenu: FC<SidebarProps> = ({ isOpen, onClose}) => {
   return (
-    <div className={`fixed inset-y-0 h-screen left-0 z-50 w-full bg-black/50 shadow-xl ${isOpen?"translate-x-0":"-translate-x-full"} hoverEffect`}>SideMenu</div>
+    <div className={`fixed inset-y-0 h-screen left-0 z-50 w-full bg-black/50 shadow-xl ${isOpen?"translate-x-0":"-translate-x-full"} hoverEffect`}>
+        <div className='min-w-72 max-w-96 bg-black h-screen p-10 border-r border-r-shop_light_green flex  flex-col gap-6'>
+            <div><Logo className='text-white'/>
+            <button><X /></button>
+            </div>
+            <div></div>
+        </div>
+    </div>
   )
 }
 
