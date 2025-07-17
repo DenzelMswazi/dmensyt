799a335adc5eaa1e3b4e71c4af22ea1668ed6de4
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-07-17 11:22:03 +0300

    feat: create a sidebar menu component

--- components/Header.tsx
+++ components/Header.tsx
@@ -13,7 +13,7 @@ const Header = () => {
     <header className='bg-white py-5 border-b border-gray-200'>
         <Container className='flex items-center justify-between text-lightColor'>
             {/* Logo */}
-            <div className='w-auto md:w-1/3 flex items-center gap-2.5 justify-start'>
+            <div className='w-auto md:w-1/3 flex items-center gap-2.5 justify-start md:gap-0'>
                <MobileMenu />
               <Logo />
               </div>
--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -1,12 +1,17 @@
+"use client";
+
 import { AlignLeft } from 'lucide-react';
-import React from 'react'
+import React, { useState } from 'react'
+import SideMenu from './SideMenu';
 
 const MobileMenu = () => {
+  const [isSideBarOpen, setIsSideBarOpen] = useState(false);
   return (
     <>
     <button>
-        <AlignLeft className='hover:text-darkColor hoverEffect md:hidden hover:cursor-pointer md:gap-0'/>
+        <AlignLeft className='hover:text-darkColor hoverEffect md:hidden hover:cursor-pointer'/>
     </button>
+    <SideMenu />
     </>
   )
 }
