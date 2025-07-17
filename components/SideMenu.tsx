31d16b99628a86643df2238fde8433499748eb22
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-07-17 13:34:08 +0300

    chore: add a transform effect for the header

--- components/SideMenu.tsx
+++ components/SideMenu.tsx
@@ -7,7 +7,7 @@ interface SidebarProps {
 
 const SideMenu: FC<SidebarProps> = ({ isOpen, onClose}) => {
   return (
-    <div>SideMenu</div>
+    <div className={`fixed inset-y-0 h-screen left-0 z-50 w-full bg-black/50 shadow-xl ${isOpen?"translate-x-0":"-translate-x-full"} hoverEffect`}>SideMenu</div>
   )
 }
 
