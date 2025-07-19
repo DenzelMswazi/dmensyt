79cfa03ae85fafd2bdae22fa0a0066658673c9cc
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-07-19 20:12:17 +0300

    chore: add a span design to the logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -5,7 +5,7 @@ import { cn } from '@/lib/utils';
 const Logo = ({className,spanDesign}:{className?:string,spanDesign?: string}) => {
   return (
     <Link href={"/"}>
-        <h2 className='{cn("text-2xl text-shop_dark_green font-black tracking-wider uppercase hover:text-shop_light_green hoverEffect group font-sans", className)}'>DMENS <span className={cn('text-shop_light_green group-hover:text-shop_dark_green', className)}>GIFT SHOP</span></h2>
+        <h2 className={cn("text-2xl text-shop_dark_green font-black tracking-wider uppercase hover:text-shop_light_green hoverEffect group font-sans", className)}>DMENS <span className={cn('text-shop_light_green group-hover:text-shop_dark_green', className)}>GIFT SHOP</span></h2>
     </Link>
   )
 }
