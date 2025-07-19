62fa623e3894152b9b57c45917ce05fe83566992
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-07-19 20:26:59 +0300

    feat: import pathname to the sidebar

--- components/SideMenu.tsx
+++ components/SideMenu.tsx
@@ -3,6 +3,7 @@ import Logo from './Logo';
 import { X } from 'lucide-react';
 import { headerData } from '@/constants/data';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 interface SidebarProps {
     isOpen: boolean;
@@ -10,6 +11,7 @@ interface SidebarProps {
 }
 
 const SideMenu: FC<SidebarProps> = ({ isOpen, onClose}) => {
+    const pathname = usePathname();
   return (
     <div className={`fixed inset-y-0 h-screen left-0 z-50 w-full bg-black/50 text-white shadow-xl ${isOpen?"translate-x-0":"-translate-x-full"} hoverEffect`}>
         <div className='min-w-72 max-w-96 bg-black h-screen p-10 border-r border-r-shop_light_green flex  flex-col gap-6'>
