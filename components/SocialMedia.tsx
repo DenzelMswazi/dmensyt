b3f06c8b060bfc6f686098d44070498c96236e20
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-07-21 12:39:32 +0300

    feat: hardcode the links

--- components/SocialMedia.tsx
+++ components/SocialMedia.tsx
@@ -1,4 +1,28 @@
+import { Facebook, Github, Instagram, Twitter } from 'lucide-react';
 import React from 'react'
+
+const socialLink = [
+    {
+        title: "instagram",
+        href: "https://www.instagram.com/dmens/",
+        icon: <Instagram className='w-5 h-5' />,
+    },
+    {
+        title: "facebook",
+        href: "https://www.facebook.com/dmensyt",
+        icon: <Facebook className='w-5 h-5' />,
+    },
+    {
+        title: "twitter",
+        href: "https://twitter.com/dmensyt",
+        icon: <Twitter className='w-5 h-5' />,
+    },
+    {
+        title: "Github",
+        href: "",
+        icon: <Github className='w-5 h-5' />,
+    }
+]
 
 const SocialMedia = () => {
   return (
