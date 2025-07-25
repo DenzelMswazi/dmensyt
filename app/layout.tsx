91533bf9c350a982c7b11d9dff36ca2f1b623f8c
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-07-25 12:00:08 +0300

    feat: add a clerk js import to the app router

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Poppins } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import {} from "@clerk/nextjs";
 
 
 const poppins = Poppins({
