17ac8380ece1d45a8186f564a5215d784b505efa
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-07-28 13:20:19 +0300

    feat: add a signin button import from clerk

--- components/SignIn.tsx
+++ components/SignIn.tsx
@@ -1,8 +1,13 @@
+import { SignInButton } from '@clerk/nextjs';
 import React from 'react'
 
 const SignIn = () => {
   return (
-    <button className='text-sm font-semibold hover:text-darkColor hover:cursor-pointer text-lightColor hoverEffect'>Login</button>
+    <SignInButton>
+      <button className='text-sm font-semibold hover:text-darkColor hover:cursor-pointer text-lightColor hoverEffect'>
+        Login
+        </button>
+    </SignInButton>
   )
 }
 
