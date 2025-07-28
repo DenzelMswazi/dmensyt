b9fc625deba0e6fd62b947a909e88231be473061
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-07-28 14:04:25 +0300

    feat: pass the login as a modal

--- components/SignIn.tsx
+++ components/SignIn.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const SignIn = () => {
   return (
-    <SignInButton>
+    <SignInButton mode='modal'>
       <button className='text-sm font-semibold hover:text-darkColor hover:cursor-pointer text-lightColor hoverEffect'>
         Login
         </button>
