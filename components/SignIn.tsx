5c8f6299fef17bd0157e7787d75e42b782061a5a
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-07-29 21:39:49 +0300

    feat: add the home banner

--- components/SignIn.tsx
+++ components/SignIn.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const SignIn = () => {
   return (
-    <SignInButton mode='modal'>
+    <SignInButton  mode='modal'>
       <button className='text-sm font-semibold hover:text-darkColor hover:cursor-pointer text-lightColor hoverEffect'>
         Login
         </button>
