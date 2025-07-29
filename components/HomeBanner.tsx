5be5b44cd14a1cf31f310d9a83d4397789a50154
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-07-29 22:01:59 +0300

    feat: add maximum min height class

--- components/HomeBanner.tsx
+++ components/HomeBanner.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const HomeBanner = () => {
   return (
-    <div className='py-16 md:py-0 bg-shop-light-pink rounded-lg'>HomeBanner</div>
+    <div className='py-16 md:py-0 bg-shop-light-pink rounded-lg px-10 lg:px-24 flex items-center justify-between'>
+        HomeBanner
+        </div>
   )
 }
 
