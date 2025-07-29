79b39224462dfe27aced85bb2f7aaaf7a637c8de
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-07-29 21:44:14 +0300

    feat: add a classname to the wrapper

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
        <html lang="en">
       <body className={`${poppins.variable} antialiased`}>
         <Header />
-        <main> 
+        <main className="flex-1"> 
           {children}
           </main>
         <Footer />
