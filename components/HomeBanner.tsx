21c1583c6beae7e55eecd7b27982ae2208ea7c24
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-07-29 22:54:27 +0300

    feat: add banner image

--- components/HomeBanner.tsx
+++ components/HomeBanner.tsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import { Title } from './ui/text';
 import Link from 'next/link';
+import Image from 'next/image';
+import { banner_1 } from '@/images';
 
 const HomeBanner = () => {
   return (
     <div className='py-16 md:py-0 bg-shop-light-pink rounded-lg px-10 lg:px-24 flex items-center justify-between'>
         <div>
             <Title>
-                Suit Up Like a Pro - Shop Dmens <br />
+                Suit Up Like a Pro <br />
                 Your Jersey. Your Team. Your Style.
             </Title>
-            <Link href={"/shop"}></Link>
+            <Link href={"/shop"} className='bg-shop_dark_green/90 text-white/90 px-5 py-2 rounded-md text-sm font-semibold hover:text-white hover:bg-shop_dark_green hovereffect'>View Products</Link>
         </div>
-        <div></div>
+        <div>
+            <Image src={banner_1} alt="banner-image" className='hidden md:inline-flex w-96'/>
+        </div>
         </div>
   )
 }
---
+++ images/index.ts
@@ -0,0 +1,3 @@
+import banner_1 from './banner_1.png';
+
+export {banner_1};
