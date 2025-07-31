38e150ccc2c6c6e196c480e060d370dfb58a96bd
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-07-31 23:11:21 +0300

    feat: pass hover effect

--- components/FooterTop.tsx
+++ components/FooterTop.tsx
@@ -11,7 +11,7 @@ interface ContactItemsData {
 const data: ContactItemsData[] = [
     {
         title: "Visit Us",
-        subTitle: "Regus Suite 205, Kiambu Road.",
+        subTitle: "Regus Suite 205, Village Market - Kiambu Road.",
         icon: (
             <MapPin className='h-6 w-6 text-gray-600 group-hover:text-primary transition-colors'/>
         ),
@@ -43,11 +43,11 @@ const FooterTop = () => {
   return (
     <div className='grid grid-cols-2 lg:grid-cols-4 gap-8 border-b'>
         {data.map((item, index) => (
-           <div key={index} className='flex items-center gap-3'>
+           <div key={index} className='flex items-center gap-3 group hover:bg-gray-50 p-4 transition-colors hoverEffect'>
              {item?.icon}
              <div>
-                <h3>{item?.title}</h3>
-                <p>{item?.subTitle}</p>
+                <h3 className='font-semibold text-gray-900 group-hover:text-black hoverEffect'>{item?.title}</h3>
+                <p className='text-gray-600 text-sm mt-1 group-hover:text-gray-900 hoverEffect'>{item?.subTitle}</p>
              </div>
            </div>
         ))}
