5afbd2de0062123b520b945b133c0acd3c287706
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-08-01 11:12:24 +0300

    feat: remove inline styling

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -13,7 +13,7 @@ const Footer = () => {
           <div className='py-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8'>
             <div className='space-y-4'>
               <Logo />
-              <SubText className='text-gray-600 text-sm'>Discover authentic jerseys, premium quality, and unbeatable prices. From legendary classics to the latest releases, we bring your team spirit to life. Shop with confidence, wear with pride.
+              <SubText>Discover authentic jerseys, premium quality, and unbeatable prices. From legendary classics to the latest releases, we bring your team spirit to life. Shop with confidence, wear with pride.
               </SubText>
               <SocialMedia className='text-darkColor/50'
                iconClassName='border-darkColor hover:border-shop_light_green hover:text-shop_light_green'
