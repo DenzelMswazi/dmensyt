94489f35db5448d09e1e237a5a2658be57b1793e
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-08-01 11:02:31 +0300

    feat: add a subtext function

--- components/ui/text.tsx
+++ components/ui/text.tsx
@@ -5,4 +5,10 @@ const Title = ({children, className}: { children:React.ReactNode; className?: st
     <h2 className={cn("text-3xl font-semibold text-shop_dark_green capitalize tracking-wide font-sans", className)}>{children}</h2>
 );
 };
+
+const subText = () => {
+    return (
+        <p>{children}</p>
+    )
+}
 export { Title };
