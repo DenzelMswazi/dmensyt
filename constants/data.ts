7ba177d75c8a4f05d5c59a4b8c884892e6a3b07b
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-08-02 04:33:21 +0300

    feat: add footer links

--- constants/data.ts
+++ constants/data.ts
@@ -5,3 +5,22 @@ export const headerData = [
     { title: 'Contact', href: '/contact' },
     { title: 'Deals', href: '/deals' },
 ];
+export const quickLinksData = [
+    { title: 'About Us', href: '/about' },
+    { title: 'Contact Us', href: '/contact' },
+    { title: 'Terms & Conditions', href: '/terms' },
+    { title: 'Privacy Policy', href: '/privacy' },
+    { title: 'FAQs', href: '/faqs' },   
+    { title: 'Help', href: '/help' },
+];
+export const categoriesData = [
+    { title: 'Jerseys', href: '/jerseys' },
+    { title: 'Shoes', href: '/shoes' },
+    { title: 'RETRO', href: '/retro' },
+    { title: 'Training kit', href: '/training' },
+    { title: 'Hoodies', href: '/hoodies' },
+    { title: 'Merchandize', href: '/merchandize' },
+    { title: 'Badges', href: '/badges' },
+    { title: 'Accessories', href: '/accessories' },
+    { title: 'Collectibles', href: '/collectibles' },
+]
