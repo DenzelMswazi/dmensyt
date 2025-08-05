82ec249bb474dbfc1b31461f0b88c2af9690dfc5
Denzel Mswazi <denzelmwangi94@gmail.com>
2025-08-05 10:10:07 +0300

    feat: add a flex property to the main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
       <body className={`${poppins.variable} antialiased`}>
         <div className="flex flex-col min-h-screen">
         <Header />
-        <main> 
+        <main className="flex-1"> 
           {children}
           </main>
         <Footer />
